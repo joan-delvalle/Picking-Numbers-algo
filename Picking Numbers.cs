256c1ce3d0b9d6f208ac870db1fe78610b55f0ef
joan-delvalle <joandelvalle865@gmail.com>
2025-10-02 00:27:45 -0700

    Picking Numbers.cs

---
+++ Picking Numbers.cs
@@ -0,0 +1,73 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<int> a)
+    {
+        Dictionary<int, int> freq = new Dictionary<int, int>();
+
+    foreach (int num in a)
+    {
+        if (!freq.ContainsKey(num))
+            freq[num] = 0;
+        freq[num]++;
+    }
+
+    int maxLen = 0;
+
+    foreach (var kvp in freq)
+    {
+        int current = kvp.Value;
+
+        if (freq.ContainsKey(kvp.Key + 1))
+        {
+            current += freq[kvp.Key + 1];
+        }
+
+        maxLen = Math.Max(maxLen, current);
+    }
+
+    return maxLen;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.pickingNumbers(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
